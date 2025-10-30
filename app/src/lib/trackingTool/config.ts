b1f86056d4f287acdfdfffb23736524dd97aadf1
Luís Marques <l.marques@sap.com>
2025-10-30 14:53:52 +0000

    Enhance classification documentation by adding a detailed description of the classification process and including download links for source code, while also updating tracking events for classification downloads.

--- app/src/lib/trackingTool/config.ts
+++ app/src/lib/trackingTool/config.ts
@@ -88,6 +88,7 @@ export const TRACKING_EVENTS = {
     DOWNLOAD_DEPLOYMENT_CUSTOM_PREDICTIVE_MODELS: "btn-download-deployment-custom-predictive-models",
     DOWNLOAD_REGRESSION: "btn-download-regression",
     DOWNLOAD_TIME_SERIES_FORECASTING: "btn-download-time-series-forecasting",
-    DOWNLOAD_ANOMALY_DETECTION: "btn-download-anomaly-detection"
+    DOWNLOAD_ANOMALY_DETECTION: "btn-download-anomaly-detection",
+    DOWNLOAD_CLASSIFICATION: "btn-download-classification"
   }
 } as const;
